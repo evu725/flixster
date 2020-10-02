fb6ae226e36a8473b1092789e929f46bdea8a38e
Emily Vu <evu725@gmail.com>
2020-10-01 22:23:17 -0400

    Update for part 2 of this project

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Emily Vu on 9/30/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
